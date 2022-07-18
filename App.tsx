0c4d7fca291a0b3f27cadc2afa7526f4737cb156
MatheusNunes <matheusnunes9147@gmail.com>
2022-07-18 19:03:41 -0300

    initial project

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <SignIn />
+  );
+};
