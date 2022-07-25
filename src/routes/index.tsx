26bf8253bd0a2478cdc431b53f0707daf4faacb4
MatheusNunes <matheusnunes9147@gmail.com>
2022-07-25 03:56:44 -0300

    project finish

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,34 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+
 
 import { SignIn } from "../screens/SignIn";
 import { AppRoutes } from "./app.routes";
+import { Loading } from "../components/Loading";
 
 export function Routes(){
+
+	const [ loading, setIsLoading] = useState(true);
+	const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+	useEffect(() => {
+		const subscriber = auth()
+		.onAuthStateChanged(response => {
+			setUser(response);
+			setIsLoading(false);
+		});
+
+		return subscriber;
+	}, []);
+
+	if(loading){
+		return <Loading />
+	}
+
 	return(
 		<NavigationContainer>
-			<AppRoutes />
+			{user ? <AppRoutes /> : <SignIn />}
 		</NavigationContainer>
 	);
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,27 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore'
+import { Alert } from 'react-native';
 import { Heading, HStack, IconButton, Text, useTheme, VStack, FlatList, Center} from 'native-base';
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 import { Filter } from '../components/Filter';
 import { Order, OrderProps } from '../components/Order';
 import { Button } from '../components/Button';
+import { dateFormat } from '../utils/firestoreDateFormat';
+import { Loading } from '../components/Loading';
 
 import Logo from '../assets/img/logo_secondary.svg'
+import { isLoading } from 'expo-font';
 
 
 export function Home() {
+	const [isloading, setIsLoading] = useState(true);
 
 	const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
 	
-	const [orders , setOrders] = useState<OrderProps[]>([{
-		id: '123',
-		patrimony: '123456',
-		when: '18/07/2022 às 14:00',
-		status: 'open'
-		}
-	]); 	
+	const [orders , setOrders] = useState<OrderProps[]>([]); 	
 
 	const {colors} = useTheme();
 
@@ -33,6 +34,37 @@ export function Home() {
 	function handleOpenDetails(orderId: string){
 		navigation.navigate('details', {orderId});
 	}
+
+	function handleLogout(){
+		auth()
+		.signOut()
+		.catch(error => {
+			console.log(error);
+			return Alert.alert('Sair', 'Não foi possível sair.')
+		});
+	}
+
+	useEffect(() =>{
+		setIsLoading(true);
+		const subscriber = firestore()
+		.collection('orders')
+		.where ('status','==', statusSelected)
+		.onSnapshot(snapshot => {
+			const data = snapshot.docs.map(doc => {
+				const {patrimony, description, status, created_at} = doc.data();
+				return {
+					id: doc.id,
+					patrimony,
+					description,
+					status,
+					when: dateFormat(created_at)
+				}
+			})
+			setOrders(data);
+			setIsLoading(false);
+		})
+		return subscriber;
+	}, [statusSelected]);
 
   return (
 	<VStack flex={1} pb={6} bg="gray.700">
@@ -49,6 +81,7 @@ export function Home() {
 
 			<IconButton
 				icon={<SignOut size={26} color={colors.gray[300]} />}
+				onPress={handleLogout}
 			/>
 		</HStack>
 
@@ -75,6 +108,8 @@ export function Home() {
 					isActivate={statusSelected === 'closed'}
 				/>
 			</HStack>
+			{
+			isLoading ? <Loading /> : 
 			<FlatList 
 				data={orders}
 				keyExtractor = {item => item.id}
@@ -91,6 +126,7 @@ export function Home() {
 					</Center>
 				)} 
 			/>
+			}
 
 			<Button title="Nova solicitação" onPress={handleNerwOrder}/>
 		</VStack>
